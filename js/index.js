3c452983959690bb01e036ff8965495a63edd7c6
HajauIsMyName <hieuhau4@gmail.com>
2023-08-19 17:21:20 +0700

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 renderThanks("Hey everyone, thanks for visiting my website! I just want use this space to thank everyone so much!");
+checkThanksBox();
 
 function renderThanks(word) {
     const thanksBox = document.getElementById("text");
@@ -11,4 +12,17 @@ function renderThanks(word) {
             thanksBox.style.color = "cyan";
         }, i * 75);
     }
+}
+
+function checkThanksBox() {
+    var checkBox = document.getElementById("check");
+    var thanksBox = document.querySelector(".thanksBox");
+
+    checkBox.addEventListener("click", () => {
+        if (checkBox.checked) {
+            thanksBox.style.display = "none";
+        } else {
+            thanksBox.style.display = "block";
+        }
+    });
 }
