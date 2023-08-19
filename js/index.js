3305b9f0181d01fbdcb7c327727c963e2079b1de
HajauIsMyName <hieuhau4@gmail.com>
2023-08-19 16:59:10 +0700

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,48 +1,14 @@
-renderText(["Welcome to my web 👋", "I'm Lâm Hậu"], "text", ["cyan", "gold"]);
+renderThanks("Hey everyone, thanks for visiting my website! I just want use this space to thank everyone so much!");
 
-function renderText(words, id, colors) {
-    if (colors === undefined) colors = ["#fff"];
-    var visible = true;
-    var con = document.getElementById("console");
-    var letterCount = 1;
-    var x = 1;
-    var waiting = false;
-    var target = document.getElementById(id)
-    target.setAttribute("style", "color:" + colors[0])
-    window.setInterval(function () {
-        if (letterCount === 0 && waiting === false) {
-            waiting = true;
-            target.innerHTML = words[0].substring(0, letterCount)
-            window.setTimeout(function () {
-                var usedColor = colors.shift();
-                colors.push(usedColor);
-                var usedWord = words.shift();
-                words.push(usedWord);
-                x = 1;
-                target.setAttribute("style", "color:" + colors[0])
-                letterCount += x;
-                waiting = false;
-            }, 1000)
-        } else if (letterCount === words[0].length + 1 && waiting === false) {
-            waiting = true;
-            window.setTimeout(function () {
-                x = -1;
-                letterCount += x;
-                waiting = false;
-            }, 1000)
-        } else if (waiting === false) {
-            target.innerHTML = words[0].substring(0, letterCount)
-            letterCount += x;
-        }
-    }, 120)
-    window.setInterval(function () {
-        if (visible === true) {
-            con.className = "text-underscore hidden"
-            visible = false;
+function renderThanks(word) {
+    const thanksBox = document.getElementById("text");
 
-        } else {
-            con.className = "text-underscore"
-            visible = true;
-        }
-    }, 400)
+    thanksBox.textContent = "";
+
+    for (let i = 0; i < word.length; i++) {
+        setTimeout(() => {
+            thanksBox.textContent += word[i];
+            thanksBox.style.color = "cyan";
+        }, i * 75);
+    }
 }
